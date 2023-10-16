9e916ff947404be058d0ab2c90ab7c2ff4909c9c
andyydao <andrewdao.w@gmail.com>
2023-10-16 14:05:55 +0100

    adjusted fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ['Satoshi', "sans-serif"],
-      inter: ['Inter', "sans-serif"]
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./components/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./nuxt.config.{js,ts}",
+	],
+	theme: {
+		fontFamily: {
+			sans: ["Plus Jakarta Sans", "sans-serif"],
+			gabarito: ["Gabarito", "sans-serif"],
+		},
+	},
+	plugins: [],
+};
