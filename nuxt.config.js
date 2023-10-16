27c5cc48d12b0dd6f6da09ee0f3d5d71daf0a77b
andyydao <andrewdao.w@gmail.com>
2023-10-16 14:55:21 +0100

    updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: ["@/assets/css/main.css"],
+	css: ["~/assets/css/main.css"],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	content: [
 		"./pages/**/*.vue",
+		"./components/**/*.vue", // Make sure to include the components directory
+		"./assets/css/*.css", // Include the CSS files in the assets directory
 		"./plugins/**/*.{js,ts}",
 		"./nuxt.config.{js,ts}",
 	],
