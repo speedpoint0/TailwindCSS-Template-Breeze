04d72b2207e731edacc1b1be575edef78db0c4a1
andyydao <andrewdao.w@gmail.com>
2023-10-16 14:13:51 +0100

    adjusting fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-	content: [
-		"./components/**/*.{js,vue,ts}",
-		"./layouts/**/*.vue",
-		"./pages/**/*.vue",
-		"./plugins/**/*.{js,ts}",
-		"./nuxt.config.{js,ts}",
-	],
+	content: [],
 	theme: {
 		fontFamily: {
 			sans: ["Sulphur Point", "sans-serif"],
