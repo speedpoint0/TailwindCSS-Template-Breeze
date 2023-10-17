d346a8b3638eca2296f8a6bd2d77e04338ae6f8f
andyydao <andrewdao.w@gmail.com>
2023-10-17 10:36:00 +0100

    removed purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-	content: [
-		"./pages/**/*.vue",
-		"./components/**/*.vue", // Make sure to include the components directory
-		"./assets/*.css", // Include the CSS files in the assets directory
-		"./plugins/**/*.{js,ts}",
-		"./nuxt.config.{js,ts}",
-	],
 	theme: {
 		extend: {
 			fontFamily: {
